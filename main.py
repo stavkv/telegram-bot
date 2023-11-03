4cf2397b891b4c30750ea6d754ecf9e9ef8958a2
mrvsingh <singhvikal891@gmail.com>
2023-11-03 21:10:26 +0530

    first commit

--- main.py
+++ main.py
@@ -8,24 +8,34 @@ TOKEN = os.getenv("TOKEN")
 
 
 def start(update, context):
-    update.message.reply_text("Ram Ram Bhai")
+    update.message.reply_text("Hello, How may I help You?")
 
 def helps(update, context):
     update.message.reply_text(
         """
-        Ram Ram Bhai,
+        How are you?
         
-        Aap ko kya krna hai??
+        what do you want to do??
         
-        /start - to start baat-cheet
-        /contact - aap mujhe main kr skte h
-        /helps - to get madad        
-        Bhagwan aapka bhala kre...
+        /start - to start the bot.
+        /contact - how you can contact me.
+        /payment - for paying the amount.
+        /link - to get the link for details
+        /helps - to get help.
+        
+        
+        Come back again...
         """
     )
     
+def link(update, context):
+    update.message.reply_text("https://falana.in")
+
+def payment(update, context):
+    update.message.reply_text("falana_upi@paytm")
+    
 def contact(update, context):
-    update.message.reply_text("Ram Ram Bhai, Mail at Totatoatota")
+    update.message.reply_text("Mail at falana@gmail.com")
 
 
 
@@ -35,7 +45,8 @@ dispatch = updater.dispatcher
 dispatch.add_handler(telegram.ext.CommandHandler('start', start))
 dispatch.add_handler(telegram.ext.CommandHandler('helps', helps))
 dispatch.add_handler(telegram.ext.CommandHandler('contact', contact))
-
+dispatch.add_handler(telegram.ext.CommandHandler('payment', payment))
+dispatch.add_handler(telegram.ext.CommandHandler('link', link))
 
 
 updater.start_polling()
